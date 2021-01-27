21bfd32f2683ffdc6a5f2bfd503777079dec6255
Mohamed Eldeeb <41117194+moeldeeb98@users.noreply.github.com>
2021-01-27 20:20:05 +0200

    create navbar coponents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = () => {
+  return <h1>Navbar</h1>;
+};
+
+export default Nav;
